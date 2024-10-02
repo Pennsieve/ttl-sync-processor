ce052d0ed3fe536bb0d6f74396f1a390e403be10
John Frommeyer <johnfr@seas.upenn.edu>
2024-10-02 17:24:50 -0400

    Adding assertions to main Run test.

--- service/processor/processor_test.go
+++ service/processor/processor_test.go
@@ -1,11 +1,14 @@
 package processor
 
 import (
+	"encoding/json"
 	"github.com/google/uuid"
+	changesetmodels "github.com/pennsieve/ttl-sync-processor/client/changeset/models"
 	"github.com/pennsieve/ttl-sync-processor/service/logging"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"log/slog"
+	"os"
 	"testing"
 )
 
@@ -25,6 +28,24 @@ func TestCurationExportSyncProcessor_Run(t *testing.T) {
 	require.NoError(t, processor.Run())
 
 	assert.FileExists(t, processor.ChangesetFilePath())
+
+	// Check changes contents
+	changesetFile, err := os.Open(processor.ChangesetFilePath())
+	defer changesetFile.Close()
+	var changeset changesetmodels.Dataset
+	require.NoError(t, json.NewDecoder(changesetFile).Decode(&changeset))
+
+	modelChanges := changeset.Models
+	assert.Len(t, modelChanges, 1)
+	contributorChanges := modelChanges[0]
+	// model exists, so ID should be present and Create nil
+	assert.Equal(t, "d77470bb-f39d-49ee-aa17-783e128cdfa0", contributorChanges.ID)
+	assert.Nil(t, contributorChanges.Create)
+
+	assert.True(t, contributorChanges.Records.DeleteAll)
+	assert.Len(t, contributorChanges.Records.Create, 3)
+	assert.Empty(t, contributorChanges.Records.Update)
+	assert.Empty(t, contributorChanges.Records.Delete)
 }
 
 func TestCurationExportSyncProcessor_ReadExistingPennsieveMetadata(t *testing.T) {
